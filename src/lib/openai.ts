7bc2f0e7610489d8fd5af488a23c62181e389af1
Shitanshu Kumar <shitanshu.kumar@impactanalytics.co>
2025-01-11 01:13:05 +0530

    api key for chat gpt changes format

--- src/lib/openai.ts
+++ src/lib/openai.ts
@@ -1,7 +1,7 @@
 import { Configuration, OpenAIApi } from "openai-edge";
 
 const config = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+  apiKey: `${process.env.OPENAI_API_KEY}`,
 });
 
 const openai = new OpenAIApi(config);
